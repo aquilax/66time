6716e2396abb5c155301c4fbbc7d0c9f9863b60b
aquilax <aquilax@gmail.com>
2018-10-21 11:32:55 +0200

    Added sample app

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/aquilax/time66"
+)
+
+func main() {
+	var lat = flag.Float64("lat", 0, "Latitude")
+	var lon = flag.Float64("lon", 0, "Longitude")
+	var offset = flag.Float64("offset", 0, "UTC Offset")
+
+	t, err := time66.GetTime(*lat, *lon, *offset, time.Now())
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(t.Format(time.RFC3339))
+}
