d69bf7bcfbef41dc1efcc39915f5090a7151a54f
aquilax <aquilax@gmail.com>
2018-11-25 05:59:21 +0100

    parse flags

--- cmd/main.go
+++ cmd/main.go
@@ -12,6 +12,7 @@ func main() {
 	var lat = flag.Float64("lat", 0, "Latitude")
 	var lon = flag.Float64("lon", 0, "Longitude")
 	var offset = flag.Float64("offset", 0, "UTC Offset")
+	flag.Parse()
 
 	t, err := time66.GetTime(*lat, *lon, *offset, time.Now())
 	if err != nil {
